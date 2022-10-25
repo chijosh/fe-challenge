85bb340e7818ca865f682883bc5a237f139c0ed0
chijosh <chijosh2010@hotmail.com>
2022-10-25 18:42:21 +0300

    add basic app components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Cart } from './containers';
+import { Products } from './components';
 
 function App() {
-  return <div>Fe-challenge</div>;
+  return (
+    <div>
+      <Cart />
+      <Products />
+    </div>
+  );
 }
 
 export default App;
